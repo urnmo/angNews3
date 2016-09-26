48695da79a2629908cb3f5022030a29c88447744
urnmo <aaron0moore@gmail.com>
2016-09-26 16:02:44 -0400

    hide button working. NYT button almost working

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 let app = angular.module("newsApp", []);
 
 app.component('articles', {
+    controller: "NewsItemController",
     templateUrl: 'components/article.html',
     bindings: {
         readable: '<',
@@ -8,33 +9,56 @@ app.component('articles', {
 });
 
 //create controller
-app.controller("NewsController", function($scope, NewsGetter){
+app.controller("NewsController", function ($scope, NewsGetter) {
     $scope.news = NewsGetter.getNews();
+    $scope.times = function () {
+        $scope.news = NewsGetter.getNYT()
+    }
+    console.log('controlly starter')
+});
 
-$scope.addToStarred = function(){
-    console.log('working?');
-//function needs to add articles to the starred group
-//where must starred group exist? a component
+app.controller("NewsItemController", function ($scope, NewsGetter) {
+    $scope.addToStarred = function (article) {
+        console.log("hay!");
+    }
 
+    $scope.hide = function (x) {
+        x.hide = true;
+        console.log("sup");
+    }
 
-};
 });
 
 //create a factory that connects to the api
-app.factory("NewsGetter", function ($http){
-//create empty array for news objects
-let newsy = [];
-$http({
-    method: 'GET',
-    url: "http://puzzlegram.herokuapp.com/news",
-}).then(function (response){
-    angular.copy(response.data.news, newsy);
-    console.log(response);
-});
-    return{
-        getNews: function(){
-            console.log(newsy);
-            return newsy;
+app.factory("NewsGetter", function ($http) {
+    //create empty array for news objects
+    let newsy = [];
+    $http({
+        method: 'GET',
+        url: "http://puzzlegram.herokuapp.com/news",
+    }).then(function (response) {
+        angular.copy(response.data.news, newsy);
+        console.log(response);
+        for (let i = 0; i < newsy.length; i++) {
+            newsy[i].hide = false;
+
         }
+    });
+    return {
+        getNYT: function () {
+            for (let i = 0; i < newsy.length; i++) {
+                if (newsy[i].publisher.name === "New York Times") {
+                    newsy[i].hide = false;
+                } else {
+                    newsy[i].hide = true;
+                }
+            }
+        },
+       
+        getNews: function () {
+            // console.log(newsy);
+            return newsy;
+        },
+
     };
 });
